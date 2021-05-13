e7347c3388ea6c988c4beceef92a6a164b61b024
Yohan Jhaveri <yohanjhaveri@gmail.com>
2021-05-12 23:26:16 -0400

    Added firebase config file

---
+++ src/database/firebase.js
@@ -0,0 +1,17 @@
+import firebase from "firebase";
+
+const app = firebase.initializeApp({
+  apiKey: "AIzaSyB0_PyqZxFZm8t0jY3PAFyP8oMxFalCYGA",
+  authDomain: "studyfind-researcher.firebaseapp.com",
+  databaseURL: "https://studyfind-researcher.firebaseio.com",
+  projectId: "studyfind-researcher",
+  storageBucket: "studyfind-researcher.appspot.com",
+  messagingSenderId: "434311866185",
+  appId: "1:434311866185:web:2794d11ef74480179f1c98",
+});
+
+const auth = app.auth();
+const storage = app.storage();
+const firestore = app.firestore();
+
+export { auth, storage, firestore };
