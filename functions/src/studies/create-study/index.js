16334e663532dc5272e42d71aa5ae100f6057ab7
Yohan Jhaveri <yohanjhaveri@gmail.com>
2021-05-13 12:02:52 -0400

    Refactored create-study function

---
+++ functions/src/studies/create-study/index.js
@@ -0,0 +1,45 @@
+const admin = require("firebase-admin");
+const cleanStudy = require("./clean-study");
+const fetchStudy = require("./fetch-study");
+const generateQuestions = require("./generate-questions");
+const firestore = admin.firestore();
+
+const ensureNewStudy = async (nctID) => {
+  const study = await firestore.collection("studies").doc(nctID);
+
+  if (study && study.published) {
+    throw Error(`Study with nctID '${nctID}' already exists in database`);
+  }
+};
+
+const checkOwnership = (contactEmail, userEmail) => {
+  const userEmailLower = userEmail.toLowerCase();
+  const contactEmailLower = contactEmail.toLowerCase();
+
+  if (userEmailLower !== contactEmailLower) {
+    throw Error("Ownership cannot be verified");
+  }
+};
+
+const createStudy = async (data, context) => {
+  try {
+    const { uid, email } = context.auth;
+    const { nctID } = data;
+
+    if (!nctID) throw Error("Parameter nctID needs to be defined");
+
+    ensureNewStudy();
+
+    const fetched = await fetchStudy(nctID);
+    const questions = generateQuestions(fetched.additionalCriteria);
+
+    checkOwnership(fetched.contactEmail, email);
+
+    const cleaned = cleanStudy(fetched, { questions, uid, email });
+    return await firestore.collection("studies").doc(nctID).set(cleaned);
+  } catch (e) {
+    return { error: e.message };
+  }
+};
+
+export default createStudy;
