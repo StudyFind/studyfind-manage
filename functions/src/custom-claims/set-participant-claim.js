0cc27bb6d6c5be5fa8b816e5b1d813a629dbc56e
Yohan Jhaveri <yohanjhaveri@gmail.com>
2021-05-23 06:34:54 -0400

    Added custom claims

---
+++ functions/src/custom-claims/set-participant-claim.js
@@ -0,0 +1,10 @@
+const admin = require("firebase-admin");
+
+module.exports = (_, context) => {
+  const auth = admin.auth();
+  const { uid, customClaims } = context.auth;
+
+  if (!customClaims.usertype) {
+    auth.setCustomUserClaims(uid, { usertype: "participant" });
+  }
+};
