8a3ea5916de8c906e15c9188129dde9ba9bb1e28
Yohan Jhaveri <yohanjhaveri@gmail.com>
2021-05-26 12:56:02 -0400

    Fixed custom claims

--- functions/src/custom-claims/set-researcher-claim.js
+++ functions/src/custom-claims/set-researcher-claim.js
@@ -2,9 +2,10 @@ const admin = require("firebase-admin");
 
 module.exports = (_, context) => {
   const auth = admin.auth();
-  const { uid, customClaims } = context.auth;
+  const { uid } = context.auth;
+  const { usertype, email_verified } = context.auth.token;
 
-  if (!customClaims.usertype) {
+  if (!email_verified && !usertype) {
     auth.setCustomUserClaims(uid, { usertype: "researcher" });
   }
 };
