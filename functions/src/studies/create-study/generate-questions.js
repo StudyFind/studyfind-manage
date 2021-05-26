699a12523c90dc37de30ef616a820efaf779cf63
Yohan Jhaveri <yohanjhaveri@gmail.com>
2021-05-26 11:53:58 -0400

    Refactored

--- functions/src/studies/create-study/generate-questions.js
+++ functions/src/studies/create-study/generate-questions.js
@@ -1,16 +1,6 @@
-const InclusionsMatcher = /(?:inclusions?\s*criteria:)((?:\n|.)*?)(?:exclusions?\s*criteria:)/gi;
-const ExclusionsMatcher = /(?:exclusions? criteria:)((?:\n|.)*?)$/gi;
-const SubpointMatcher = /(\n.*:\n(?:\n|.)*)(?:\n\n)/gi;
-
-const generateSurvey = (criteria) => {
-  const inclusionRaw = criteria.matchAll(InclusionsMatcher).next().value;
-  const exclusionRaw = criteria.matchAll(ExclusionsMatcher).next().value;
-
-  return {
-    inclusion: inclusionRaw ? makeCriteria(inclusionRaw[1]) : [],
-    exclusion: exclusionRaw ? makeCriteria(exclusionRaw[1]) : [],
-  };
-};
+const inclusionsMatcher = /(?:inclusions?\s*criteria:)((?:\n|.)*?)(?:exclusions?\s*criteria:)/gi;
+const exclusionsMatcher = /(?:exclusions? criteria:)((?:\n|.)*?)$/gi;
+const subpointMatcher = /(\n.*:\n(?:\n|.)*)(?:\n\n)/gi;
 
 const clean = (array) => {
   return array.split("\n").filter((i) => i.trim() !== "");
@@ -21,19 +11,23 @@ const flatten = (subpoints) => {
 };
 
 const makeCriteria = (s) => {
-  const points = s.replace(SubpointMatcher, (_, $1) => {
-    const subpoints = clean($1);
-    return flatten(subpoints);
-  });
-
+  const points = s.replace(subpointMatcher, (_, $1) => flatten(clean($1)));
   return clean(points);
+};
+
+const generateSurvey = (criteria) => {
+  const inclusionRaw = criteria.matchAll(inclusionsMatcher).next().value;
+  const exclusionRaw = criteria.matchAll(exclusionsMatcher).next().value;
+
+  return {
+    inclusion: inclusionRaw ? makeCriteria(inclusionRaw[1]) : [],
+    exclusion: exclusionRaw ? makeCriteria(exclusionRaw[1]) : [],
+  };
 };
 
 module.exports = (criteria) => {
   const { inclusion, exclusion } = generateSurvey(criteria);
-
   const inclusionList = inclusion.map((prompt) => ({ type: "Inclusion", prompt }));
   const exclusionList = exclusion.map((prompt) => ({ type: "Exclusion", prompt }));
-
   return inclusionList.concat(exclusionList);
 };
