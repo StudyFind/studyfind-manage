0cb6ad6b88a57fcdc3842b04d83b5787e8d8997e
Yohan Jhaveri <yohanjhaveri@gmail.com>
2021-06-09 16:59:41 -0400

    Refactored on-create-study

--- functions/src/notifications/triggered/on-create-study.js
+++ functions/src/notifications/triggered/on-create-study.js
@@ -2,27 +2,32 @@ const { firestore } = require("admin");
 const { getDocument } = require("utils");
 const { CREATE_STUDY } = require("../__utils__/notification-codes");
 
+const sendEmailNotification = async () => {
+  return firestore.collection("mail").add({
+    to: "yohan@studyfind.org",
+    message: {
+      subject: "New Study Created",
+      text: "A new study has been created in your account",
+    },
+  });
+};
+
+const sendDatabaseNotification = async (researcherID, studyID) => {
+  return firestore.collection("researchers").doc(researcherID).collection("notifications").add({
+    time: Date.now(),
+    code: CREATE_STUDY,
+    meta: { studyID },
+  });
+};
+
 module.exports = async (snapshot) => {
   const studyID = snapshot.id;
   const researcherID = snapshot.get("researcher.id");
 
   const researcher = await getDocument(firestore.collection("researchers").doc(researcherID));
   const preference = researcher.notifications.categories.studies;
-  
-  admin.firestore().collection('mail').add({
-    to: 'yohan@studyfind.org',
-    message: {
-      subject: 'New Study Created',
-      text: 'A new study has been created in your account',
-    }
-  }).then(() => console.log('send email already'));
-  
-  return (
-    preference &&
-    firestore.collection("researchers").doc(researcherID).collection("notifications").add({
-      time: Date.now(),
-      code: CREATE_STUDY,
-      meta: { studyID },
-    })
-  );
+
+  if (preference) {
+    await Promise.all([sendEmailNotification(), sendDatabaseNotification(researcherID, studyID)]);
+  }
 };
