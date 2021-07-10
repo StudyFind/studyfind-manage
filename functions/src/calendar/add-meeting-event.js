dc529d92ec58f7b0f1cf1d2510e7ffcae6c2da43
Yohan Jhaveri <yohanjhaveri@gmail.com>
2021-07-10 19:11:58 -0400

    Temporary

--- functions/src/calendar/add-meeting-event.js
+++ functions/src/calendar/add-meeting-event.js
@@ -1,7 +1,8 @@
 const { firestore } = require("admin");
 const { getDocument } = require("utils");
 const { google } = require("googleapis");
-const OAuth2 = google.auth.OAuth2;
+const { OAuth2 } = google.auth;
+
 const calendar = google.calendar("v3");
 const moment = require("moment-timezone");
 
@@ -9,22 +10,24 @@ function convertToGoogleTimestamp(timestamp, timezone) {
   return moment.unix(timestamp).tz(timezone).format("YYYY-MM-DDTHH:mm:ss");
 }
 
-module.exports.addEventToCalendar = async (snapshot) => {
-  const researcherID = snapshot.get("researcherID");
-  const meetingName = snapshot.get("name");
-  const meetingTime = snapshot.get("time");
+module.exports = async (data, context) => {
+  const { meetingID } = data;
+  const { uid } = context.auth;
 
-  const researcherDoc = await getDocument(firestore.collection("researchers").doc(researcherID));
-  const timezone = researcherDoc.timezone;
+  const [meeting, researcher] = Promise.all([
+    await getDocument(firestore.collection("meetings").doc(meetingID)),
+    await getDocument(firestore.collection("researchers").doc(uid)),
+  ]);
 
-  const startTime = convertToGoogleTimestamp(meetingTime / 1000, timezone);
-  const endTime = convertToGoogleTimestamp(meetingTime / 1000 + 30 * 60, timezone);
+  const name = meeting.get("name");
+  const time = meeting.get("time");
 
-  const eventData = {
-    eventName: meetingName,
-    startTime,
-    endTime,
-  };
+  const timezone = researcher.timezone;
+
+  const startTime = convertToGoogleTimestamp(time / 1000, timezone);
+  const endTime = convertToGoogleTimestamp(time / 1000 + 30 * 60, timezone);
+
+  const eventData = { name, startTime, endTime };
 
   const oAuth2Client = new OAuth2(
     "624561481612-m54akpcb260eu2robisp8bs95sc2l4eg.apps.googleusercontent.com",
@@ -45,6 +48,7 @@ module.exports.addEventToCalendar = async (snapshot) => {
   const urlParams = new URLSearchParams(window.location.search);
   const code = urlParams.get("code");
   const { tokens } = await oAuth2Client.getToken(code);
+
   oAuth2Client.setCredentials({
     refresh_token: tokens.refresh_token,
   });
@@ -55,11 +59,11 @@ module.exports.addEventToCalendar = async (snapshot) => {
 function addEvent(event, auth, timezone) {
   calendar.events.insert(
     {
-      auth: auth,
+      auth,
       calendarId: "Primary",
       resource: {
         summary: "studyfind meeting reminder",
-        description: event.evenvtName,
+        description: event.eventName,
         start: {
           dateTime: event.startTime,
           timeZone: timezone,
