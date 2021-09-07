f037b852c77ef7c031d32bcc8ba53192697c5147
DAVID CHEN <davidchen1716@gmail.com>
2021-09-06 20:12:53 -0400

    create researcher acc test

--- functions/src/notifications/triggered/researcher-account/on-create-researcher-account.js
+++ functions/src/notifications/triggered/researcher-account/on-create-researcher-account.js
@@ -1,17 +1,31 @@
 const { auth, firestore } = require("admin");
 const { CREATE_ACCOUNT } = require("../../__utils__/notification-codes");
+const sendEmail = require("../../__utils__/send-email");
 
 module.exports = async (snapshot) => {
   const researcherID = snapshot.id;
   const researcherName = snapshot.get("name");
+  const researcherEmail = snapshot.get("email");
 
   await auth.setCustomUserClaims(researcherID, { usertype: "researcher" });
 
-  // TODO: Send welcome email
+  // TODO: Send welcome email, promise all
+  await sendEmail(
+    researcherEmail,
+    "Create researcher account subject",
+    "Create researcher account text"
+  );
 
-  return firestore.collection("researchers").doc(researcherID).collection("notifications").add({
-    time: Date.now(),
-    code: CREATE_ACCOUNT,
-    meta: { researcherName },
-  });
+  return firestore
+    .collection("researchers")
+    .doc(researcherID)
+    .collection("notifications")
+    .add({
+      code: CREATE_ACCOUNT,
+      time: Date.now(),
+      link: "",
+      title: "Researcher account created!",
+      description: `Your account has been successfully created as ${researcherName}!`,
+      read: false,
+    });
 };
