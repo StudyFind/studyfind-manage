0c20a041cf0461694938cfcdeae815278bb8c1dd
DAVID CHEN <davidchen1716@gmail.com>
2021-09-08 13:22:36 -0400

    delete study notif

--- functions/src/notifications/triggered/study/on-delete-study.js
+++ functions/src/notifications/triggered/study/on-delete-study.js
@@ -1,14 +1,25 @@
-const onTriggerStudy = require("./on-trigger-study");
-module.exports = async (snapshot) => onTriggerStudy(snapshot, "CREATE_STUDY");
+const { auth } = require("admin");
+const { DELETE_STUDY } = require("../../__utils__/notification-codes");
+const { getResearcher, addResearcherNotification } = require("../../__utils__/database");
+const sendEmail = require("../../__utils__/send-email");
 
-/*
+module.exports = async (snapshot) => {
+  const studyID = snapshot.id;
+  const study = snapshot.data();
+  const researcherID = study.researcher.id;
 
-Researcher
-----------
-title: "Study Deleted"
-body: "You deleted study ${meta.study.id}`
-icon: FiFileMinus
-color: "red.500"
-background: "red.100"
+  const researcher = await getResearcher(researcherID);
 
-*/
+  if (researcher?.notifications?.email) {
+    const user = await auth.getUser(researcherID);
+    const researcherEmail = user.email;
+    await sendEmail(researcherEmail, "Delete study subject", "Delete study text");
+  }
+
+  return addResearcherNotification(
+    researcherID,
+    DELETE_STUDY,
+    "Study Deleted",
+    `You deleted study ${studyID}.`
+  );
+};
