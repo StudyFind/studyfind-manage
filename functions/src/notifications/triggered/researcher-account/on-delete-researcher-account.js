ea5cea0a4fa0be0bffa0805bc0ca574bb4ec92da
jsisson7 <70162294+jsisson7@users.noreply.github.com>
2021-09-15 18:30:44 -0400

    Most recent maintenence functions

--- functions/src/notifications/triggered/researcher-account/on-delete-researcher-account.js
+++ functions/src/notifications/triggered/researcher-account/on-delete-researcher-account.js
@@ -1,1 +1,28 @@
+const { firestore } = require("admin");
+
+module.exports = async (snap, context) => {
+  //Delete all the researchers studies
+
+  //1. Get doc id to determine which studies to delete
+  //const data = snap.data();
+  const uid = snap.id;
+  //2. create a query that selects documents owned by that id
+
+  // const studiesRef = collection(firestore, "studies");
+  // const q = query(studiesRef, where("researcher.id", "==", uid));
+
+  const q = firestore.collection("studies").where("researcher.id", "==", uid);
+  //3. iterate through studies in the query and delete them
+  q.get()
+    .then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        doc.ref.delete();
+      });
+      return true;
+    })
+    .catch(() => {
+      return false;
+    });
+};
+
 // TODO: Send goodbye email
