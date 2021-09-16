7803ba83d71883a02c21dd9de4592963fd5cbe5a
Yohan Jhaveri <yohanjhaveri@gmail.com>
2021-09-16 11:08:51 -0400

    Refactored

--- src/ResetPassword.js
+++ src/ResetPassword.js
@@ -1,7 +1,8 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { Grid, Heading, Button } from "@chakra-ui/react";
-import { Message, TextInput, Form } from "@studyfind/components";
+import { Card, Form, Message, TextInput } from "@studyfind/components";
 import { useFirebase } from "@studyfind/firebase";
+import { validate } from "@studyfind/utils";
 
 function ResetPassword({ code }) {
   const [show, setShow] = useState(false);
@@ -10,32 +11,20 @@ function ResetPassword({ code }) {
   const [loading, setLoading] = useState(false);
   const [success, setSuccess] = useState(null);
   const { auth } = useFirebase();
-
   const handleToggle = () => {
     setShow((show) => !show);
   };
 
-  const check = (password) => {
-    const checkCase = password !== password.toLowerCase();
-    const checkSize = password.length > 7;
-
-    if (!password) return " ";
-    if (!checkCase && !checkSize) return "Password must have 8 characters and one capital letter";
-    if (!checkCase) return "Password must have a capital letter";
-    if (!checkSize) return "Password must have 8 characters";
-    return "";
-  };
-
   const handleChange = (_, value) => {
     setInputs({ password: value });
-    setErrors({ password: check(value) });
+    setErrors({ password: validate.password(value) });
   };
 
   const handleSubmit = async () => {
     try {
       setLoading(true);
 
-      const error = check(inputs.password);
+      const error = validate.password(inputs.password);
 
       if (error) {
         setErrors({ password: error });
@@ -66,29 +55,31 @@ function ResetPassword({ code }) {
 
   return (
     <Form onSubmit={handleSubmit}>
-      <Grid p="30px" gap="20px" bg="#f8f9fa" borderRadius="5px" borderWidth="1px" w="350px">
-        <Heading fontSize="1.75rem" mb="6px" color="blue.500" textAlign="center">
-          Reset Password
-        </Heading>
-        <TextInput
-          size="lg"
-          name="password"
-          value={inputs.password}
-          error={errors.password}
-          onChange={handleChange}
-          placeholder="Password"
-          type={show ? "text" : "password"}
-          rightWidth="5rem"
-          right={
-            <Button color="gray.500" size="sm" onClick={handleToggle}>
-              {show ? "Hide" : "Show"}
-            </Button>
-          }
-        />
-        <Button size="lg" colorScheme="blue" type="submit" isLoading={loading}>
-          Confirm Reset Password
-        </Button>
-      </Grid>
+      <Card p="30px" w="350px">
+        <Grid gap="20px">
+          <Heading fontSize="1.75rem" mb="6px" color="blue.500" textAlign="center">
+            Reset Password
+          </Heading>
+          <TextInput
+            size="lg"
+            name="password"
+            value={inputs.password}
+            error={errors.password}
+            onChange={handleChange}
+            placeholder="Password"
+            type={show ? "text" : "password"}
+            rightWidth="5rem"
+            right={
+              <Button color="gray.500" size="sm" onClick={handleToggle}>
+                {show ? "Hide" : "Show"}
+              </Button>
+            }
+          />
+          <Button size="lg" colorScheme="blue" type="submit" isLoading={loading}>
+            Confirm Reset Password
+          </Button>
+        </Grid>
+      </Card>
     </Form>
   );
 }
--- src/VerifyEmail.js
+++ src/VerifyEmail.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { Flex, Spinner } from "@chakra-ui/react";
 import { Message } from "@studyfind/components";
 import { useFirebase } from "@studyfind/firebase";
