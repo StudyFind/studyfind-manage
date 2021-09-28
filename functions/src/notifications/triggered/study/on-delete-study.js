a73da763fdb8a736799ede29c41a84a8d32f4242
DAVID CHEN <davidchen1716@gmail.com>
2021-09-28 00:22:48 -0400

    study phone notifs

--- functions/src/notifications/triggered/study/on-delete-study.js
+++ functions/src/notifications/triggered/study/on-delete-study.js
@@ -2,24 +2,36 @@ const { auth } = require("admin");
 const { DELETE_STUDY } = require("../../__utils__/notification-codes");
 const { getResearcher, addResearcherNotification } = require("../../__utils__/database");
 const sendEmail = require("../../__utils__/send-email");
+const sendPhone = require("../../__utils__/send-phone");
 
 module.exports = async (snapshot) => {
-  const studyID = snapshot.id;
   const study = snapshot.data();
   const researcherID = study.researcher.id;
+
+  const subject = "Study Deleted!";
+  const text = `${study.title} has been deleted.`;
 
   const researcher = await getResearcher(researcherID);
 
   if (researcher?.notifications?.email) {
     const user = await auth.getUser(researcherID);
     const researcherEmail = user.email;
-    await sendEmail(researcherEmail, "Delete study subject", "Delete study text");
+    await sendEmail(
+      researcherEmail,
+      subject,
+      `${text}\n To unsubscribe from these notifications, please visit: https://studyfind-researcher.firebaseapp.com/account/notifications/`
+    );
   }
 
-  return addResearcherNotification(
-    researcherID,
-    DELETE_STUDY,
-    "Study Deleted",
-    `${studyID} has been deleted.`
-  );
+  if (researcher?.notifications?.phone) {
+    const researcherPhone = researcher.phone;
+    researcherPhone &&
+      /\d\d\d\d\d\d\d\d\d\d/.test(researcherPhone) &&
+      (await sendPhone(
+        `+1${researcherPhone}`,
+        `${text}\n To unsubscribe visit: https://studyfind-researcher.firebaseapp.com/account/notifications/`
+      ));
+  }
+
+  return addResearcherNotification(researcherID, DELETE_STUDY, subject, text);
 };
