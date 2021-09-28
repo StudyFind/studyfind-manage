57b8c2475dd30142318eb6f126ec6bd6b5eb4e6e
DAVID CHEN <davidchen1716@gmail.com>
2021-09-28 01:18:23 -0400

    studyparticipant phone notifs

--- functions/src/notifications/triggered/study-participant/on-participant-enrolled.js
+++ functions/src/notifications/triggered/study-participant/on-participant-enrolled.js
@@ -3,27 +3,43 @@ const { getDocument } = require("utils");
 const { PARTICIPANT_ENROLLED } = require("../../__utils__/notification-codes");
 const { getResearcher, addResearcherNotification } = require("../../__utils__/database");
 const sendEmail = require("../../__utils__/send-email");
+const sendPhone = require("../../__utils__/send-phone");
 
 module.exports = async (snapshot, context) => {
-  const participantID = snapshot.id;
-
+  const studyParticipant = snapshot.data();
   const { studyID } = context.params;
   const study = await getDocument(firestore.collection("studies").doc(studyID));
-
   const researcherID = study.researcher.id;
   const researcher = await getResearcher(researcherID);
+
+  const subject = "New participant enrollment";
+  const text = `${studyParticipant.fakename} has enrolled in study ${studyID}`;
 
   if (researcher?.notifications?.email) {
     const user = await auth.getUser(researcherID);
     const researcherEmail = user.email;
-    await sendEmail(researcherEmail, "Participant enrolled subject", "Participant enrolled text");
+    await sendEmail(
+      researcherEmail,
+      subject,
+      `${text}: ${`/study/${studyID}/participants`}\n To unsubscribe from these notifications, please visit: https://studyfind-researcher.firebaseapp.com/account/notifications/`
+    );
+  }
+
+  if (researcher?.notifications?.phone) {
+    const researcherPhone = researcher.phone;
+    researcherPhone &&
+      /\d\d\d\d\d\d\d\d\d\d/.test(researcherPhone) &&
+      (await sendPhone(
+        `+1${researcherPhone}`,
+        `${text}: ${`/study/${studyID}/participants`}\n To unsubscribe visit: https://studyfind-researcher.firebaseapp.com/account/notifications/`
+      ));
   }
 
   return addResearcherNotification(
     researcherID,
     PARTICIPANT_ENROLLED,
-    "New participant enrollment",
-    `${participantID} has enrolled in ${studyID}!`,
+    subject,
+    text,
     `/study/${studyID}/participants`
   );
 };
--- functions/src/notifications/triggered/study-participant/on-researcher-changed-participant-status.js
+++ functions/src/notifications/triggered/study-participant/on-researcher-changed-participant-status.js
@@ -2,30 +2,43 @@ const { auth } = require("admin");
 const { RESEARCHER_CHANGED_PARTICIPANT_STATUS } = require("../../__utils__/notification-codes");
 const { getParticipant, addParticipantNotification } = require("../../__utils__/database");
 const sendEmail = require("../../__utils__/send-email");
+const sendPhone = require("../../__utils__/send-phone");
 
 module.exports = async (change, context) => {
   const before = change.before.get("status");
   const after = change.after.get("status");
   if (before === after) return undefined;
   const { studyID, participantID } = context.params;
-
   const participant = await getParticipant(participantID);
+
+  const subject = "Researcher Changed Participant Status!";
+  const text = `Your status in study ${studyID} is now ${after}`;
 
   if (participant?.notifications?.email) {
     const user = await auth.getUser(participantID);
     const participantEmail = user.email;
     await sendEmail(
       participantEmail,
-      "Researcher changed participant status subject",
-      "Researcher changed participant status text"
+      subject,
+      `${text}: ${`/mystudies/${studyID}/`}\n To unsubscribe from these notifications, please visit: https://studyfind-researcher.firebaseapp.com/account/notifications/`
     );
+  }
+
+  if (participant?.notifications?.phone) {
+    const participantPhone = participant.phone;
+    participantPhone &&
+      /\d\d\d\d\d\d\d\d\d\d/.test(participantPhone) &&
+      (await sendPhone(
+        `+1${participantPhone}`,
+        `${text}: ${`/mystudies/${studyID}/`}\n To unsubscribe visit: https://studyfind-researcher.firebaseapp.com/account/notifications/`
+      ));
   }
 
   return addParticipantNotification(
     participantID,
     RESEARCHER_CHANGED_PARTICIPANT_STATUS,
-    "Study participant status changed",
-    `Your status for study ${studyID} has changed from ${before} to ${after}.`,
+    subject,
+    text,
     `/mystudies/${studyID}/`
   );
 };
